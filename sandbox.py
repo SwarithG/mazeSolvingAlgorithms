0fc2cd704a115c14184a8d92d93fb471f5a1296f
Swarith Gandham <swarith.gandham@gmail.com>
2025-10-11 14:16:52 -0400

    practice commit

---
+++ sandbox.py
@@ -0,0 +1,12 @@
+adjust = (0,1)
+quant = [12,3]
+print(quant)
+print(type(quant))
+# quant = quant + adjust
+print(quant)
+
+
+row,column = 2,3
+print(row,column)
+row,column = row+adjust[0],column+adjust[1]
+print(row,column)
